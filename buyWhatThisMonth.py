f72fd33b93267e9a083dfc46966a108bfbbc7208
hsu414 <hsuyichen1994@gmail.com>
2022-03-27 13:23:09 +0200

    feat: buyWhatThisMonth

--- buyWhatThisMonth.py
+++ buyWhatThisMonth.py
@@ -1,5 +1,6 @@
 #KamiiGTAA
 #import necessary libraries
+#pip install lib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas_datareader as web
@@ -16,6 +17,7 @@ def startTime_calc(period, start='none'):
     else:
         this_month = int(start.split('-')[1])
         this_year = int(start.split('-')[0])
+    print(this_month,this_year)
  
     start_month = this_month - period
     start_year = this_year
@@ -30,10 +32,10 @@ def startTime_calc(period, start='none'):
        end_month = 12
        end_year = this_year - 1
         
-    if(start_month <= 0):
+    while(start_month <= 0):
         start_month = 12 + start_month 
         start_year = this_year - 1 
-        string_start = calc_String(start_month)
+    string_start = calc_String(start_month)
         
     if(end_month == 4 or end_month == 6 or end_month == 9 or end_month == 11):
         end_date = '30'
@@ -44,7 +46,7 @@ def startTime_calc(period, start='none'):
                
     start = str(start_year) + '-' + string_start + '-' + start_date
     end = str(end_year) + '-' + string_end + '-' + end_date
-               
+
     return start, end
 
 #macht Datum zweistellig 1.8 - 01.08
@@ -57,11 +59,6 @@ def calc_String(numb):
         string = str(numb)
     
     return string
-
-
-
-tickers = ['VTV', 'MTUM', 'VBR', 'DWAS', 'EFA', 'EEM', 'IEF', 'IGOV', 'LQD', 'TLT', 'GSG', 'IAU', 'VNQ']
-
 
 
 def geFinancetData(tickers,start,end):
@@ -82,8 +79,8 @@ def calNMonthlyPerformanceMean(monthlyAvgPrice, months):
 
     for n in months:
         monthlyPerformance= monthlyPerformance.append((monthlyAvgPrice.iloc[-1]-monthlyAvgPrice.iloc[-1-n])*100/monthlyAvgPrice.iloc[-1-n],ignore_index=True)
-    print('monthlyPerformance')
-    print(monthlyPerformance)
+    #print('monthlyPerformance')
+    #print(monthlyPerformance)
    
     return monthlyPerformance.mean()
 
@@ -97,7 +94,8 @@ def findBestCandidates(acc, cur):
 
 def buyWhatThisMonth(tickers, this_month):
     # getFinanceData
-    [start, end] =  startTime_calc(13, this_month)   
+    [start, end] =  startTime_calc(13, this_month)
+    print(start, end) 
     financeRecords= geFinancetData(tickers, start, this_month )
     financeRecords= financeRecords['Adj Close']
 
@@ -108,10 +106,10 @@ def buyWhatThisMonth(tickers, this_month):
     currentPrices = financeRecords.tail(1)
     financeRecords.drop(financeRecords.tail(1).index,inplace=True)
     monthlyAvgPrice = financeRecords.groupby(pd.Grouper(freq='M')).mean()
-    print('MonthlyAvgPrice')
-    print(monthlyAvgPrice)
-    print('CurrentPrice')
-    print(currentPrices)
+    #print('MonthlyAvgPrice')
+    #print(monthlyAvgPrice)
+    #print('CurrentPrice')
+    #print(currentPrices)
 
      
 
@@ -119,15 +117,15 @@ def buyWhatThisMonth(tickers, this_month):
 
     #determine who has the best performance
     top3= monthlyPerformanceMean.sort_values(axis='index', ascending = False).iloc[0:3]
-    print(top3)
- 
-
-
+    #print(top3)
     
 
-    return
+    return currentPrices[top3.index]
 
 
-buyWhatThisMonth(tickers, '2021-06-01')
+
+tickers = ['VTV', 'MTUM', 'VBR', 'DWAS', 'EFA', 'EEM', 'IEF', 'IGOV', 'LQD', 'TLT', 'GSG', 'IAU', 'VNQ']
+#buyWhatThisMonth(tickers, '2019-01-01')
+financeRecords= web.get_data_yahoo(tickers, '2020-12-01', '2021-01-01' )
 
 
